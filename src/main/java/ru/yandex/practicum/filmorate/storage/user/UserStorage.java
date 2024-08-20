da568943b5377424f558a8632ed9a84f786474b4
Mamaeva <dmamaieva6055@yandex.ru>
2024-08-20 10:54:25 +0300

    sprint 12 fixed code style

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -7,11 +7,18 @@ import java.util.List;
 
 public interface UserStorage {
     Collection<User> getAll();
+
     User add(User user);
+
     User update(User user);
+
     User getUserById(Long userId);
+
     void addFriend(Long userId, Long friendId);
+
     void deleteFriend(Long userId, Long friendId);
+
     List<User> getFriendsByUserId(Long id);
+
     List<User> getCommonFriends(Long userId, Long friendId);
 }
